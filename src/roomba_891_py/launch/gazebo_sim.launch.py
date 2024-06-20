4dece3edb324b438918a6cae602195f3835566b3
falOn-Dev <falon.n.clark@gmail.com>
2024-06-20 00:28:04 -0400

    Publishing it all for stack exchange

---
+++ src/roomba_891_py/launch/gazebo_sim.launch.py
@@ -0,0 +1,48 @@
+import os
+from ament_index_python import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+import xacro
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+
+def generate_launch_description():
+
+    pkg_name = 'roomba_891_py'
+    file_subpath = 'description/robot_base.urdf.xacro'
+
+    xacro_file = os.path.join(get_package_share_directory(pkg_name),file_subpath)
+    robot_description_raw = xacro.process_file(xacro_file).toxml()
+
+    gazebo = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('ros_gz_sim'), 'launch'), '/gz_sim.launch.py']),
+                    launch_arguments={
+                        'pause' : 'true',
+                        'gz_args' : 'empty.sdf',
+                    }.items(),
+                )
+    
+    create_entity = Node(package='ros_gz_sim',
+                    executable='create',
+                    arguments=['-topic', '/robot_description',
+                                '-entity', 'robot',
+                                '-z', '1.0',],
+                    output='screen')
+    
+    node_robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[{'robot_description': robot_description_raw}, {'use_sim_time': True}] # add other parameters here if required
+    )
+
+    
+
+
+    return LaunchDescription([
+        gazebo,
+        create_entity,
+        node_robot_state_publisher,
+    ])
